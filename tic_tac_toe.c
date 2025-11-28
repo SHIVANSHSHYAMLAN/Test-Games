db438d9b6257133ac3e1e54244223c813e0d7376
SHIVANSHSHYAMLAN <shivanshshyamlan@gmail.com>
2025-11-28 19:26:59 +0530

    Completed the 2 player tic-tac-toe game

--- tic_tac_toe.c
+++ tic_tac_toe.c
@@ -1,7 +1,5 @@
 #include<stdio.h>
 #include<stdlib.h>
- int input_user;  //needs only 4 inputs ----> 5th will be automatic
- int input_user2;  //input by the second user
 char whole_steup[3][3]={{'0','1','2'},{'3','4','5'},{'6','7','8'}};
 void checker(int input){
     if((input<0)||(input>8)){
@@ -27,6 +25,36 @@ void checker(int input){
       }
 }
     }
+void winning(void){
+ for(int i = 0; i<3; i++){
+      if((whole_steup[i][0]==whole_steup[i][1])&&(whole_steup[i][0]==whole_steup[i][2])&&(whole_steup[i][1]=='*')){
+        printf("One with the * won the game");
+        exit(0);
+      }
+     else if((whole_steup[0][i]==whole_steup[1][i])&&(whole_steup[0][i]==whole_steup[2][i])&&(whole_steup[i][i]=='*')){
+        printf("One with the * won the game");
+        exit(0);
+      }
+      else  if((whole_steup[i][0]==whole_steup[i][1])&&(whole_steup[i][0]==whole_steup[i][2])&&(whole_steup[i][1]=='#')){
+        printf("One with the # won the game");
+        exit(0);
+      }
+     else if((whole_steup[0][i]==whole_steup[1][i])&&(whole_steup[0][i]==whole_steup[2][i])&&(whole_steup[i][i]=='#')){
+        printf("One with the # won the game");
+       exit(0);
+      }  
+      else{}
+ }
+     if((whole_steup[0][0]==whole_steup[1][1])&&(whole_steup[0][0]==whole_steup[2][2])&&(whole_steup[1][1]=='*')){
+        printf("One with the * won the game");
+        exit(0);
+      }
+    else  if((whole_steup[0][0]==whole_steup[1][1])&&(whole_steup[0][0]==whole_steup[2][2])&&(whole_steup[1][1]=='#')){
+        printf("One with the # won the game");
+        exit(0);
+      }
+      else{}
+    }
 int main(){
      //printing the outline of the game
     int number = 0;
@@ -39,7 +67,8 @@ int main(){
     }
 //Now working on the input scenario
 
-   
+ int input_user;  //needs only 4 inputs ----> 5th will be automatic
+ int input_user2;  //input by the second user
         printf("Enter the input as per the value assign ");
      scanf("%d",&input_user);
      checker(input_user);
@@ -77,7 +106,6 @@ int main(){
         }
         printf("\n");
     }
-         checker(input_user2);
   //Completed till the second user fist time
   //Completed
       printf("Enter the input as per the value assign ");
@@ -101,7 +129,7 @@ int main(){
 //Printed till the second user input
       printf("Enter the input as per the value assign ");
      scanf("%d",&input_user2);
-     checker(input_user2);
+      checker(input_user2);
     if(input_user2<=2){
         whole_steup[0][input_user2] = '#';
     }
@@ -117,8 +145,89 @@ int main(){
         }
         printf("\n");
     }
-         checker(input_user2);
+   
   //Completed till the second user second time
   //Completed
+    printf("Enter the input as per the value assign ");
+     scanf("%d",&input_user);
+     checker(input_user);
+    if(input_user<=2){
+        whole_steup[0][input_user] = '*';
+    }
+    else if((input_user>2)&&(input_user<=5)){
+        whole_steup[1][input_user-3] = '*';
+    }
+    else  whole_steup[2][input_user-6] = '*';
+            
+    //Completed till the second input by the first user
+    for(int i= 0; i<3; i++){
+        for(int j=0;j<3;j++){
+      printf("%c\t",whole_steup[i][j]);
+        }
+        printf("\n");
+    }
+//Printed till the second user input
+      printf("Enter the input as per the value assign ");
+     scanf("%d",&input_user2);
+      checker(input_user2);
+    if(input_user2<=2){
+        whole_steup[0][input_user2] = '#';
+    }
+    else if((input_user>2)&&(input_user2<=5)){
+        whole_steup[1][input_user2-3] = '#';
+    }
+    else  whole_steup[2][input_user2-6] = '#';
+            
+    //Completed till the second input by the second and first user
+    for(int i= 0; i<3; i++){
+        for(int j=0;j<3;j++){
+      printf("%c\t",whole_steup[i][j]);
+        }
+        printf("\n");
+    }
+   winning();
+  //Completed till the second user second time
+  //Completed
+    printf("Enter the input as per the value assign ");
+     scanf("%d",&input_user);
+     checker(input_user);
+    if(input_user<=2){
+        whole_steup[0][input_user] = '*';
+    }
+    else if((input_user>2)&&(input_user<=5)){
+        whole_steup[1][input_user-3] = '*';
+    }
+    else  whole_steup[2][input_user-6] = '*';
+            
+    //Completed till the second input by the first user
+    for(int i= 0; i<3; i++){
+        for(int j=0;j<3;j++){
+      printf("%c\t",whole_steup[i][j]);
+        }
+        printf("\n");
+    }
+//Printed till the second user input
+      printf("Enter the input as per the value assign ");
+     scanf("%d",&input_user2);
+      checker(input_user2);
+    if(input_user2<=2){
+        whole_steup[0][input_user2] = '#';
+    }
+    else if((input_user>2)&&(input_user2<=5)){
+        whole_steup[1][input_user2-3] = '#';
+    }
+    else  whole_steup[2][input_user2-6] = '#';
+            
+    //Completed till the second input by the second and first user
+    for(int i= 0; i<3; i++){
+        for(int j=0;j<3;j++){
+      printf("%c\t",whole_steup[i][j]);
+        }
+        printf("\n");
+    }
+   winning();
+  //Completed till the second user second time
+  //Completed
+  printf("No one won the game, its a tie one");
     return(0);
 }
